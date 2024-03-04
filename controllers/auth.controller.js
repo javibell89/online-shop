eb94365d129fe1b96699796fa91ffa5caaad04a1
jbellido89 <149900298+jbellido89@users.noreply.github.com>
2024-03-04 17:10:12 +0100

    Adding base CSS files and static file serving

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,9 @@
 function getSignup(req, res) {
-  res.render('auth/signup');
+  res.render('customer/auth/signup');
 }
 
 function getLogin(req, res) {
-  res.render('auth/login');
+  res.render('customer/auth/login');
 }
 
 module.exports = {
