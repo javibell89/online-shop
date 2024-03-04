0682f85081254d65ebb11a7133041f737741232f
jbellido89 <149900298+jbellido89@users.noreply.github.com>
2024-03-04 16:32:17 +0100

    formatted

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,9 @@
 function getSignup(req, res) {
-  res.render("auth/signup");
+  res.render('auth/signup');
 }
 
 function getLogin(req, res) {
-  res.render("auth/login");
+  res.render('auth/login');
 }
 
 module.exports = {
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 
-const authController = require("../controllers/auth.controller");
+const authController = require('../controllers/auth.controller');
 
 const router = express.Router();
 
-router.get("/signup", authController.getSignup);
-router.get("/login", authController.getLogin);
+router.get('/signup', authController.getSignup);
+router.get('/login', authController.getLogin);
 
 module.exports = router;
